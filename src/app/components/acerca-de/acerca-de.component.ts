6dcd728bb9e5a9409e53971aa2b49765e3969abd
tiburon-blanco <antolini.martin@gmail.como>
2023-04-20 14:12:39 -0300

    m

--- src/app/components/acerca-de/acerca-de.component.ts
+++ src/app/components/acerca-de/acerca-de.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
 @Component({
   selector: 'app-acerca-de',
   templateUrl: './acerca-de.component.html',
   styleUrls: ['./acerca-de.component.css']
 })
-export class AcercaDeComponent {
+export class AcercaDeComponent implements OnInit {
+  myPorfolio: any;
+  constructor(private datosPorfolio: PortfolioService) { }
+
+
+
+  ngOnInit(): void {
+    this.datosPorfolio.obtenerDatos().subscribe(data => {
+      console.log(data);
+      this.myPorfolio = data
+    });
+
+  }
 
 }
+
+
