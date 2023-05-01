e9eb321ba5ea5abcf5245c9f95cedf04a79bbd6a
Tiburon_Blanco <antolini.martin@gmail.com>
2023-05-01 18:12:42 -0300

    FIX

--- src/app/components/experiencia/experiencia.component.ts
+++ src/app/components/experiencia/experiencia.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { Experiencia } from 'src/app/model/experiencia.model';
 import { ExperienciaService } from 'src/app/servicios/experiencia.service';
+import { formatDate } from '@angular/common' 
+
 declare var window: any;
 
 @Component({
@@ -73,27 +75,28 @@ export class ExperienciaComponent implements OnInit {
   }
 
   openModalExperienciaEdit(experiencia: any): void {
-
-     this.formulario.controls['id'].value = experiencia!.id;
-     this.formulario.controls['logo'].value = experiencia!.logo;
-     this.formulario.controls['empresa'].value = experiencia!.empresa;
-     this.formulario.controls['puesto'].value = experiencia.puesto;
-     this.formulario.controls['desde'].value = experiencia.desde; 
-     this.formulario.controls['hasta'].value = experiencia.hasta;
-     this.formulario.controls['descripcion'].value = experiencia.descripcion;
+     console.log(experiencia);
+     this.formulario.controls['id'].setValue(experiencia!.id);
+     this.formulario.controls['logo'].setValue(experiencia!.logo);
+     this.formulario.controls['empresa'].setValue(experiencia!.empresa);
+     this.formulario.controls['puesto'].setValue(experiencia.puesto);
+     this.formulario.controls['desde'].setValue(formatDate(experiencia.desde,'yyyy-MM-dd','en')); 
+     this.formulario.controls['hasta'].setValue(formatDate(experiencia.hasta,'yyyy-MM-dd','en'));
+     this.formulario.controls['descripcion'].setValue(experiencia.descripcion);
      this.formModal.show();
   }
 
   updateExperiencia(): void {
+    this.experiencia.id = this.formulario.controls['id'].value;
     this.experiencia.logo = this.formulario.controls['logo'].value;
     this.experiencia.empresa = this.formulario.controls['empresa'].value
     this.experiencia.puesto = this.formulario.controls['puesto'].value
     this.experiencia.desde = this.formulario.controls['desde'].value
     this.experiencia.hasta = this.formulario.controls['hasta'].value
     this.experiencia.descripcion = this.formulario.controls['descripcion'].value
-    this.experienciaService.addExperiencia(this.experiencia).subscribe(data => {
-      this.experiencias?.push(data);
+    this.experienciaService.updateExperiencia(this.experiencia).subscribe(data => {
       this.formModal.hide();
+      this.getExperiencias();
     });
   }
 }
